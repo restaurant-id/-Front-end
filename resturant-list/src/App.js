425fc61c67103446bfc1e29debaf4340e5566c16
SFMack <sfcamack@icloud.com>
2019-10-16 19:35:58 -0700

    branch

--- resturant-list/src/App.js
+++ resturant-list/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-
+import NavBar from './components/NavBar.js';
 
 function App() {
-  return (
-    <div className="App">
-      <NavBar/>
-      hello world
-    </div>
-  );
+	return (
+		<div className='App'>
+			<NavBar />
+			hello world
+		</div>
+	);
 }
 
 export default App;
