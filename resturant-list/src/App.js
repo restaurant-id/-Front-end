a1e966d45b028d80a557d281393af29f704767f5
grizzlybrodams <jddaley328@gmail.com$4josh@josh-pcgit config --global user.namegit config --global user.name grizzlybrodams>
2019-10-20 19:46:06 -0600

    layout done

--- resturant-list/src/App.js
+++ resturant-list/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import ResurantCard from "./Components/RestaurantCards"
 
 
 function App() {
   return (
     <div className="App">
       hello world
-     
+     <ResurantCard/>
     </div>
   );
 }
