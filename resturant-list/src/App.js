9d1156fe3c76669621ba45af6dd154f435360ce0
SFMack <sfcamack@icloud.com>
2019-10-21 20:31:10 -0700

    basic design structure

--- resturant-list/src/App.js
+++ resturant-list/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import NavBar from './components/NavBar.js';
+import RestaurantDetails from './components/RestaurantDetails/RestaurantDetails.js';
 
 function App() {
 	return (
 		<div className='App'>
 			<NavBar />
-			hello world
+			<RestaurantDetails />
 		</div>
 	);
 }
