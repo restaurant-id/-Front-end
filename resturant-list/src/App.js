c44e9805605bcda3eed4bfe7ac3bbb05c9aaf907
Yara Castro <yaracastro24@gmail.com>
2019-10-21 22:15:29 -0500

    Basic Nav styling

--- resturant-list/src/App.js
+++ resturant-list/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
       hello world
+      <Footer/>
     </div>
   );
 }
--- resturant-list/src/components/NavBar.js
+++ resturant-list/src/components/NavBar.js
@@ -1,9 +1,54 @@
-import React from 'react';
+import React,{useState} from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem } from 'reactstrap';
 
 export default function NavBar() {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = () => setIsOpen(!isOpen);
     return(
         <div>
-            <h2>navigation Bar</h2>
+            <Navbar color="light" light expand="md">
+                <NavbarBrand href="/">Restaurant Passport</NavbarBrand>
+                <NavbarToggler onClick={toggle} />
+                <Collapse isOpen={isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+            <NavItem>
+            <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Map
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    Restaurants Near Me 
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    Reset
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </NavItem>
+            <NavItem>
+                <NavLink href="/components/">Sign In</NavLink>
+            </NavItem>
+            <NavItem>
+                <NavLink href="/components/">
+                  <button> Sign Up</button>
+                </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
         </div>
     )
 }
