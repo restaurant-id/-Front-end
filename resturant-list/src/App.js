320d391fd1aee74930af35ebbbfa17a231d9f9e1
grizzlybrodams <jddaley328@gmail.com$4josh@josh-pcgit config --global user.namegit config --global user.name grizzlybrodams>
2019-10-22 19:55:36 -0600

    added color

--- resturant-list/src/App.js
+++ resturant-list/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import ResurantCard from "./Components/RestaurantCards"
+import ResaurantCard from "./Components/RestaurantCards"
 
 
 function App() {
   return (
     <div className="App">
       hello world
-     <ResurantCard/>
+     <ResaurantCard/>
     </div>
   );
 }
--- resturant-list/src/Components/RestaurantCards.js
+++ resturant-list/src/Components/RestaurantCards.js
@@ -7,25 +7,37 @@ import {
   CardTitle,
   CardImg,
   Row,
-  Col
+  Col,
+  CardLink
+
 } from "reactstrap";
-import Image from 'react-bootstrap/Image'
+import Image from "react-bootstrap/Image";
 
 export default function RestaurantCard(props) {
   return (
-  <Container>
-    <Card class="bg-danger">
-      <Row>
-        <Col xs={6} md={4}>
-          <Image src="https://source.unsplash.com/random" alt="Restaurant" thumbnail/>
-        </Col>
+    <Container >
+      <Card className="bg-warning">
+        <Row>
+          <Col xs={6} md={4}>
+            <Image
+              src="https://source.unsplash.com/random"
+              alt="Restaurant"
+              thumbnail
+            />
+          </Col>
           <CardBody>
-            <CardTitle>{"Restaurant name"}</CardTitle>
-            <CardText>{"Rating"}</CardText>
-            <CardText>{"location"}</CardText>
+            <CardTitle className="font-weight-bold">{"Restaurant name"}</CardTitle>
+            <CardText >{"Rating"}</CardText>
+            <CardText className="mr-5  float-right" >{"Price"}</CardText>
+            <CardText >{"Location"}</CardText>
+            <CardText className="mr-5  float-right">{"Type"}</CardText>
+            <CardLink  href="https://www.google.com"> 647 People have Recommend. Friends.......</CardLink> 
           </CardBody>
-      </Row>
-    </Card>
-  </Container>
+        </Row>
+      </Card>
+    </Container>
   );
 }
+
+
+
