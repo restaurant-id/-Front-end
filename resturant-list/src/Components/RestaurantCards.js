b5f4a3070bb63eff125a96c8d57d3af793b21610
grizzlybrodams <jddaley328@gmail.com$4josh@josh-pcgit config --global user.namegit config --global user.name grizzlybrodams>
2019-10-22 21:33:10 -0600

    okay

--- resturant-list/src/Components/RestaurantCards.js
+++ resturant-list/src/Components/RestaurantCards.js
@@ -13,17 +13,24 @@ import Image from 'react-bootstrap/Image'
 
 export default function RestaurantCard(props) {
   return (
-  <Container>
-    <Card class="bg-danger">
+    <Container >
+    <Card className="bg-warning">
       <Row>
         <Col xs={6} md={4}>
-          <Image src="https://source.unsplash.com/random" alt="Restaurant" thumbnail/>
+          <Image
+            src="https://source.unsplash.com/random"
+            alt="Restaurant"
+            thumbnail
+          />
         </Col>
           <CardBody>
-            <CardTitle>{"Restaurant name"}</CardTitle>
-            <CardText>{"Rating"}</CardText>
-            <CardText>{"location"}</CardText>
-          </CardBody>
+          <CardTitle className="font-weight-bold">{"Restaurant name"}</CardTitle>
+          <CardText >{"Rating"}</CardText>
+          <CardText className="mr-5  float-right" >{"Price"}</CardText>
+          <CardText >{"Location"}</CardText>
+          <CardText className="mr-5  float-right">{"Type"}</CardText>
+          <CardLink  href="https://www.google.com"> 647 People have Recommend. Friends.......</CardLink> 
+        </CardBody>
       </Row>
     </Card>
   </Container>
