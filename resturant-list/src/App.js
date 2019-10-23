d5781b092d13ba425aee4bf61834a7abbf268fec
grizzlybrodams <jddaley328@gmail.com$4josh@josh-pcgit config --global user.namegit config --global user.name grizzlybrodams>
2019-10-22 20:45:47 -0600

    small change

--- resturant-list/src/App.js
+++ resturant-list/src/App.js
@@ -3,7 +3,7 @@ import NavBar from './Components/NavBar';
 import Footer from './Components/Footer';
 import AppRouter from "./Components/AppRouter";
 // import Login from './Components/Login';
-// import RestaurantCard from "./Components/RestaurantCards"
+import RestaurantCard from "./Components/RestaurantCards"
 
 import 'bootstrap/dist/css/bootstrap.css';
 import "semantic-ui-css/semantic.min.css";
@@ -12,8 +12,10 @@ import "semantic-ui-css/semantic.min.css";
 function App() {
   return (
     <div className="App">
-      hello world
-     <ResaurantCard/>
+     <NavBar/>
+     <RestaurantCard/>
+     <AppRouter/>
+     <Footer/>
     </div>
   );
 }
