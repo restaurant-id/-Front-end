69a1092a6ecf1dba1a0dc77df649d7471f55de2d
Yara Castro <yaracastro24@gmail.com>
2019-10-23 21:34:02 -0500

    restore files

--- resturant-list/src/Components/RestaurantCards.js
+++ resturant-list/src/Components/RestaurantCards.js
@@ -1,37 +1,43 @@
-import React from "react";
+import React from 'react';
 import {
-  Container,
-  Card,
-  CardText,
-  CardBody,
-  CardTitle,
-  Row,
-  Col,
-  CardLink
-
-} from "reactstrap";
-import Image from "react-bootstrap/Image";
+	Container,
+	Card,
+	CardText,
+	CardBody,
+	CardTitle,
+	Row,
+	Col,
+	CardLink
+} from 'reactstrap';
+import Image from 'react-bootstrap/Image';
 
 export default function RestaurantCard(props) {
-  return (
-  <Container>
-    <Card className="bg-warning">
-      <Row>
-        <Col xs={6} md={4}>
-          <Image src="https://source.unsplash.com/random" alt="Restaurant" thumbnail/>
-        </Col>
-          <CardBody>
-          <CardTitle className="font-weight-bold">{"Restaurant name"}</CardTitle>
-            <CardText >{"Rating"}</CardText>
-            <CardText className="mr-5  float-right" >{"Price"}</CardText>
-            <CardText >{"Location"}</CardText>
-            <CardText className="mr-5  float-right">{"Type"}</CardText>
-            <CardLink  href="https://www.google.com"> 647 People have Recommend. Friends.......</CardLink> 
-          </CardBody>
-      </Row>
-    </Card>
-  </Container>
-  );
+	return (
+		<Container>
+			<Card className='bg-warning'>
+				<Row>
+					<Col xs={6} md={4}>
+						<Image
+							src='https://source.unsplash.com/random'
+							alt='Restaurant'
+							thumbnail
+						/>
+					</Col>
+					<CardBody>
+						<CardTitle className='font-weight-bold'>
+							{'Restaurant name'}
+						</CardTitle>
+						<CardText>{'Rating'}</CardText>
+						<CardText className='mr-5  float-right'>{'Price'}</CardText>
+						<CardText>{'Location'}</CardText>
+						<CardText className='mr-5  float-right'>{'Type'}</CardText>
+						<CardLink href='https://www.google.com'>
+							{' '}
+							647 People have Recommend. Friends.......
+						</CardLink>
+					</CardBody>
+				</Row>
+			</Card>
+		</Container>
+	);
 }
-
-
