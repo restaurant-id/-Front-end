a3455922963999271ccc62c3065fd7652b0d1459
grizzlybrodams <jddaley328@gmail.com$4josh@josh-pcgit config --global user.namegit config --global user.name grizzlybrodams>
2019-10-23 22:15:15 -0600

    pulled data

--- resturant-list/src/App.js
+++ resturant-list/src/App.js
@@ -3,6 +3,7 @@ import NavBar from './Components/NavBar';
 import Footer from './Components/Footer';
 import AppRouter from "./Components/AppRouter";
 // import RestaurantCard from "./Components/RestaurantCards"
+import RestaurantList from "./Components/RestaurantList"
 
 import 'bootstrap/dist/css/bootstrap.css';
 import "semantic-ui-css/semantic.min.css";
@@ -11,6 +12,7 @@ function App() {
   return (
     <div className="App">
       <NavBar/>
+      <RestaurantList/>
       <AppRouter/>
       <Footer/>
     </div>
--- resturant-list/src/Components/RestaurantList.js
+++ resturant-list/src/Components/RestaurantList.js
@@ -1,7 +1,31 @@
-import React from "reactstrap";
-import axios from "axios"
+import React,{useState, useEffect}from "react";
+import axiosWithAuth from "../utils/axiosWithAuth"
+// import RestaurantCard from "./Components/RestaurantCards.js" 
 
 
 
+export default function RestaurantList(props) {
+    const [cities, setCities] = useState([]);
 
+    useEffect(() => {
+        axiosWithAuth()
+        .get("/cities")
+        .then(res=>{
+            setCities(res.data);
+            console.log(res)
+        })
+        .catch(err => console.log(err));
+    },[]);
+
+
+    return(
+        <div>
+            {cities.map(city=>{
+                
+                console.log(city)
+            })}
+            
+        </div>
+    )
+}
 
